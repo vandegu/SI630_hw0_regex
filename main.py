3983919e3ba97896cb7a4c2d01cf82f23bd06e3e
Andrew Vande Guchte <avandegu@umich.edu>
2019-01-13 15:50:26 -0500

    ~75%

--- main.py
+++ main.py
@@ -8,5 +8,14 @@ din = pd.read_table('webpages.csv',delimiter=',')
 din = np.array(din)
 
 # Iterate over the input data and search for email addresses. If found, add to the array.
-for i,line in enumerate(din[0:4,:]):
-    print(line[0])
+for i,line in enumerate(din[:]):
+    sstr = line[0]
+    m = re.search('([a-zA-Z0-9]+)\s*(/at/|@|\[at\])\s*([a-zA-Z0-9]+)\s*(/dot/|\[dot\]|\.)\s*([a-zA-Z0-9]+)',sstr)
+    if m:
+        #print('{}@{}.{}'.format(m.group(1),m.group(3),m.group(5)))
+        din[i,1] = '{}@{}.{}'.format(m.group(1),m.group(3),m.group(5))
+    else:
+        din[i,1] = 'None'
+
+print(din[:100,:])
+np.savetxt('email-outputs.csv',din,delimiter=',',header='html,email',fmt='%s',comments='')
