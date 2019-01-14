abd5da3d9d00d8d66482071777304c254e00becd
Andrew Vande Guchte <avandegu@umich.edu>
2019-01-14 13:32:06 -0500

    79

--- main.py
+++ main.py
@@ -3,14 +3,17 @@ import pandas as pd
 import numpy as np
 import re
 
-#  Open the file with Pandas (faster, avoids the pesky 'b's)
+# Open the file with Pandas (faster, avoids the pesky 'b's)
 din = pd.read_table('webpages.csv',delimiter=',')
 din = np.array(din)
+
+# The RegEx used in our search.
+statement = '([a-zA-Z0-9]+)\s*(/at/|@|\[at\]|at)\s*([a-zA-Z0-9]+)\s*(/dot/|\[dot\]|\.|dot)\s*([a-zA-Z0-9]+)'
 
 # Iterate over the input data and search for email addresses. If found, add to the array.
 for i,line in enumerate(din[:]):
     sstr = line[0]
-    m = re.search('([a-zA-Z0-9]+)\s*(/at/|@|\[at\])\s*([a-zA-Z0-9]+)\s*(/dot/|\[dot\]|\.)\s*([a-zA-Z0-9]+)',sstr)
+    m = re.search(statement,sstr)
     if m:
         #print('{}@{}.{}'.format(m.group(1),m.group(3),m.group(5)))
         din[i,1] = '{}@{}.{}'.format(m.group(1),m.group(3),m.group(5))
