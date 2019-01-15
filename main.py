6401eb0e80e5989d435057878eed654a3926e1e2
Andrew Vande Guchte <avandegu@umich.edu>
2019-01-15 10:13:16 -0500

    95.8!

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ din = pd.read_table('webpages.csv',delimiter=',')
 din = np.array(din)
 
 # The RegEx used in our search.
-statement = '([\.a-zA-Z0-9]+)\s*(/at/|@|@@|\[at\]|at)\s*([a-zA-Z0-9]+)\s*(/dot/|\[dot\]|\.|dot)\s*([a-zA-Z0-9\.]+)'
+statement = '([a-zA-Z0-9_\-\.]+)\s*(/at/|@|@@|\[at\]|at)\s*([a-zA-Z0-9]+)\s*(/dot/|\[dot\]|\.|dot)\s*([a-zA-Z0-9\.]+)'
 
 # Iterate over the input data and search for email addresses. If found, add to the array.
 for i,line in enumerate(din[:]):
@@ -20,5 +20,5 @@ for i,line in enumerate(din[:]):
     else:
         din[i,1] = 'None'
 
-print(din[:100,:][din[:100,1]=='None'])
+print(din[100:300,:][din[100:300,1]=='None'])
 np.savetxt('email-outputs.csv',din,delimiter=',',header='html,email',fmt='%s',comments='')
